0f60eb21b23675a3ae3689dad3c7ea59512c4b4b
Markus Fritz <52305084@edu.campus02.at>
2023-09-29 16:30:48 +0200

    Initial commit

---
+++ src/Uebung_1.java
@@ -0,0 +1,10 @@
+public class Uebung_1 {
+    public static void main(String[] args) {
+        System.out.println("Markus FRITZ");
+        System.out.println("A guads Schnitzl");
+        System.out.println("Freitag \n");
+        System.out.println("Markus FRITZ \nA guads Schnitzl \nFreitag");
+        // \n = Umbruch ; Strg + d = Kopieren ; Strg + Leer = Ausfüllvorschläge
+
+    }
+}
